bc346410ab865cddb779a7eba89119e586b93eca
Ega Fernanda Putra <dragoon.fernanda12@gmail.com>
2022-12-02 11:18:50 +0700

    etl dim table from db

---
+++ extract_dimtable.py
@@ -0,0 +1,95 @@
+import sqlalchemy as db
+import pandas as pd
+from sqlalchemy import *
+
+def conndb():
+# membuat koneksi dengan database menggunakan SQLAlchemy
+    global engine,conn
+    engine = db.create_engine("mssql+pymssql://KCBISE:dbise2022@localhost:1433/ISE")  
+    conn = engine.connect()
+
+# ETL dimension table from database
+
+def dim_participant():
+    # Extracting data from database
+    dim_participant = pd.read_sql_table('Dimension_Participant', conn)
+
+    # Transform data sort alphabetical name
+    dim_sort = dim_participant.sort_values(['full_name','category','id'], ascending=(True, False, True))
+    dim_sort = dim_sort.reset_index(drop=True)
+    
+    # Load To CSV 
+    dim_sort.to_csv('csv/Dim_Participant.csv', index=False)
+     
+def dim_city():
+    # Extracting data from database
+    dim_city = pd.read_sql_table('Dimension_City', conn)
+    
+    # Transform data sort alphabetical name
+    dim_sort = dim_city.sort_values(['region'], ascending=(True))
+    dim_sort = dim_sort.reset_index(drop=True)
+    
+    # Load To CSV 
+    dim_sort.to_csv('csv/Dim_City.csv', index=False)
+    
+def dim_instance():
+    # Extracting data from database
+    dim_instance = pd.read_sql_table('Dimension_Instance', conn)
+    
+    # Transform data sort alphabetical name
+    dim_sort = dim_instance.sort_values(['name'], ascending=(True))
+    dim_sort = dim_sort.reset_index(drop=True)
+    
+    # Load To CSV 
+    dim_sort.to_csv('csv/Dim_Instance.csv', index=False)
+    
+def dim_team():
+    # Extracting data from database
+    dim_team = pd.read_sql_table('Dimension_Team', conn)
+    
+    # Transform data sort alphabetical name
+    dim_sort = dim_team.sort_values(['name'], ascending=(True))
+    dim_sort = dim_sort.reset_index(drop=True)
+    
+    # Load To CSV 
+    dim_sort.to_csv('csv/Dim_Team.csv', index=False)
+
+def dim_payment():
+    # Extracting data from database
+    dim_payment = pd.read_sql_table('Dimension_Payment', conn)
+    
+    # Load To CSV 
+    dim_payment.to_csv('csv/Dim_Payment.csv', index=False)
+
+def dim_region():
+    # Extracting data from database
+    dim_region = pd.read_sql_table('Dimension_Region', conn)
+    
+    # Load To CSV 
+    dim_region.to_csv('csv/Dim_Region.csv', index=False)
+
+def dim_source():
+    # Extracting data from database
+    dim_source= pd.read_sql_table('Dimension_Source', conn)
+    
+    # Load To CSV 
+    dim_source.to_csv('csv/Dim_Source.csv', index=False)
+
+def dim_time():
+    # Extracting data from database
+    dim_time= pd.read_sql_table('Dimension_Time', conn)
+    
+    # Load To CSV 
+    dim_time.to_csv('csv/Dim_Time.csv', index=False)
+    
+def etl_dimensiontable():
+    conndb()
+    dim_participant()
+    dim_city()
+    dim_instance()
+    dim_region()
+    dim_payment()
+    dim_team()
+    dim_source()
+    dim_time()
+    
--- main.py
+++ main.py
@@ -5,8 +5,34 @@ from participant import participant
 from sales import sales
 from team import team
 from source import source
+from extract_dimtable import etl_dimensiontable,conndb,dim_time,dim_participant,dim_city,dim_instance,dim_payment,dim_region,dim_source,dim_team
+
 
 def pre():
+    if os.path.exists('csv/Dim_City.csv'):
+        os.remove('csv/Dim_City.csv')
+    
+    if os.path.exists('csv/Dim_Participant'):
+        os.remove('csv/Dim_Participant.csv')
+    
+    if os.path.exists('csv/Dim_Instance.csv'):
+        os.remove('csv/Dim_Instance.csv')
+    
+    if os.path.exists('csv/Dim_Payment.csv'):
+        os.remove('csv/Dim_Payment.csv')
+        
+    if os.path.exists('csv/Dim_Time.csv'):
+        os.remove('csv/Dim_Time.csv')
+        
+    if os.path.exists('csv/Dim_Source.csv'):
+        os.remove('csv/Dim_Source.csv')
+        
+    if os.path.exists('csv/Dim_Team.csv'):
+        os.remove('csv/Dim_Team.csv')
+        
+    if os.path.exists('csv/Dim_Region.csv'):
+        os.remove('csv/Dim_Region.csv')
+        
     if os.path.exists('csv/Fact_Participant.csv'):
         os.remove('csv/Fact_Participant.csv')
     
@@ -19,8 +45,10 @@ def pre():
     if os.path.exists('csv/Fact_Source.csv'):
         os.remove('csv/Fact_Source.csv')
 
+
 def main():
     pre()
+    etl_dimensiontable()
     participant()
     sales()
     team()
